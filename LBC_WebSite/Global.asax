7d053a29861276bb92f314078920f4dff24b716e
pope francis ogbonna <pope francis ogbonna@POPEFRANCIS>
2018-02-15 04:03:37 +0100

    Add project files.

---
+++ LBC_WebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LBC_WebSite.MvcApplication" Language="C#" %>
---
+++ LBC_WebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LBC_WebSite.Startup))]
+namespace LBC_WebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
